0fb2ef104bb82accc476f9575b3e23ebe97b58a4
Ельвіра Матраєва <e.matrayeva@kpnu.km.ua>
2021-12-09 23:43:48 +0300

    Добавьте файлы проекта.

---
+++ Lab_9_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab_9_1
+{
+    class Trapezium
+    {
+        public void Calc()
+        {
+            Console.Write("Введiть нижню межу = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введiть верхню межу = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введiть кiлькiсть вiдрiзкiв, на яку розбивається iнтеграл = ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            double h, x1, x2, res, sum = 0;
+            h = (b - a) / n;
+            for (int i = 0; i < n; i++)
+            {
+                x1 = a + i * h;
+                x2 = a + (i + 1) * h;
+                sum += (Math.Exp(Math.Sin(x1)) + Math.Exp(Math.Sin(x2)));
+            }
+            res = sum * h / 2;
+            Console.WriteLine($"Вiдповiдь: {Math.Round(res, 3)}");
+        }
+    }
+    public delegate void Fun();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trapezium t = new Trapezium();
+            Fun x = new Fun(t.Calc);
+            x();
+        }
+    }
+}
